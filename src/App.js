03a227f0785e0fe943692b05695709cecfb61a07
Ravi <sravim589@gmail.com>
2022-06-05 03:44:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Restaurant from "./component/Basics/Restaurant";
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <Restaurant />;
+};
+
+export default App;
